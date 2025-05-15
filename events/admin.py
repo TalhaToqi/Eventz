a9340d7bb6a00a0a678cbb486340301adc68c2d8
Talha Toqi <talhatoqi1@gmail.com>
2025-05-15 20:53:23 +1000

    admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Event, Ticket
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_time', 'location', 'organizer', 'capacity', 'tickets_sold')
+    list_filter = ('category', 'start_time', 'organizer')
+    search_fields = ('title', 'description', 'location')
+
+    def tickets_sold(self, obj):
+        return obj.tickets.count()
+    tickets_sold.short_description = "Tickets Sold"
+
+admin.site.register(Category)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Ticket)
